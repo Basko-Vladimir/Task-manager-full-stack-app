3ac0141db517767e92c27996c3df25c717b13fdc
Vladimir Basko <1989bvg@gmail.com>
2024-07-19 11:34:27 +0300

    created jwt.config.ts + added some project settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,21 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    "es2021": true
+    es2021: true
   },
   plugins: ['@typescript-eslint/eslint-plugin', 'import'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended'
+  ],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.js', '*.jsx'],
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module'
+      }
+    }
   ],
   rules: {
     'prettier/prettier': 'off'
