7c6769c4544ee7652c4ccb720243021e97f207e1
Vladimir Basko <1989bvg@gmail.com>
2024-07-19 12:13:29 +0300

    created user module, jwt.strategy and prisma.service

---
+++ task-manager-backend/src/user/user.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+
+import { PrismaService } from '../prisma.service';
+
+@Injectable()
+export class UserService {
+  constructor(private prismaService: PrismaService) {}
+
+  async getUserById(id: string) {
+    return this.prismaService.user.findUnique({
+      where: { id },
+      include: { tasks: true }
+    });
+  }
+}
